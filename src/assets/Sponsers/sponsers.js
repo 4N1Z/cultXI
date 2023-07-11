0052be17a92a1658db6a95b131caa72061bcf3b1
4N1Z <20am014@sctce.ac.in>
2023-07-11 15:58:06 +0530

    sponser updates

--- src/assets/Sponsers/sponsers.js
+++ src/assets/Sponsers/sponsers.js
@@ -1,10 +1,12 @@
 import ramachandran from './ramachandrans.png';
 import tripShopping from './TripShopin.png';
 import Explorworld from './Explorworld.jpg'
+import kimsHealth from './kimsHealth.png'
 
 
 export const sponsers = [
     ramachandran,
     tripShopping,
     Explorworld,
+    kimsHealth,
 ]
